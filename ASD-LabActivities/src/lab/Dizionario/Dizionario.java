e3b3faaef4e397fc1d757e246e08ec23a2d137fe
fellon83 <f.amoruso14@studenti.uniba.it>
2014-12-08 19:31:20 +0100

    Changes to be committed: 	deleted:    ASD-LabActivities/src/lab/Dizionario/ArrayDoubling.java 	deleted:    ASD-LabActivities/src/lab/Dizionario/ArrayOrdinato.java 	deleted:    ASD-LabActivities/src/lab/Dizionario/Dizionario.java 	deleted:    ASD-LabActivities/src/lab/Dizionario/HashMapDict.java 	deleted:    ASD-LabActivities/src/lab/Dizionario/Keyboard.java 	deleted:    ASD-LabActivities/src/lab/Dizionario/StrutturaCollegata.java

--- ASD-LabActivities/src/lab/Dizionario/Dizionario.java
+++
@@ -1,9 +0,0 @@
-package lab.Dizionario;
-
-public interface Dizionario {
-	public void insert(Object e, Comparable k);
-
-	public void delete(Comparable k);
-
-	public Object search(Comparable k);
-}
